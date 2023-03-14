878c870cb6e8fa76da8305dffc4ec873ff161dc5
lcvriend <43742775+lcvriend@users.noreply.github.com>
2023-03-14 13:57:03 +0100

    Move class Data to data.js

--- src/data.js
+++ src/data.js
@@ -1,4 +1,55 @@
-export function parseCSV(csv) {
+export class Data {
+    constructor(values, headers) {
+        this.headers = headers
+        this.values = values
+        this.grouped = groupby(values)
+    }
+
+    static fromCSV(csv) {
+        let {colnames, values} = parseCSV(csv)
+        const [key, datestr, timestr, category] = colnames
+        const headers = {key, datestr, timestr, category}
+        for (const row of values) {
+            row.key = row[key]
+            row.category = row[category]
+            row.timestamp = Data.getTimestampFromDateAndTime(row, headers)
+        }
+        const groups = groupby(values)
+        for (const group of Object.values(groups)) {
+            addNextTimestamp(group)
+            addDurationInMs(group)
+        }
+        values = ungroup(groups)
+        return new Data(values, headers)
+    }
+
+    static getTimestampFromDateAndTime(row, headers) {
+        const {[headers.datestr]: date, [headers.timestr]: time} = row
+        const datestr = `${date.slice(0,4)}-${date.slice(4,6)}-${date.slice(6)}`
+        return Date.parse(`${datestr} ${time}`)
+    }
+}
+
+
+function addNextTimestamp(data) {
+    data.forEach((item, index) => {
+        const nextItem = data[index + 1]
+        item.timestamp2 = nextItem
+            ? nextItem.timestamp
+            : item.timestamp + 300000
+    })
+}
+
+
+function addDurationInMs(data) {
+    data.forEach(item => {
+        item.duration = item.timestamp2 - item.timestamp
+    })
+}
+
+
+
+function parseCSV(csv) {
     let [colnames, ...values] = csv.trim().split("\n").map(row => row.split(";").map(i => i.trim()))
     values = values.map(row => {
         return row.reduce((obj, val, idx) => {
@@ -10,7 +61,7 @@ export function parseCSV(csv) {
 }
 
 
-export function groupby(data) {
+function groupby(data) {
     const groups = data.reduce((acc, current) => {
         const {key: key, ...newObj} = current
         if (!acc[key]) {
@@ -23,7 +74,7 @@ export function groupby(data) {
 }
 
 
-export function ungroup(groups) {
+function ungroup(groups) {
     const ungrouped = []
     for (const [key, group] of Object.entries(groups)) {
         for (const row of group) {row.key = key}
--- src/sleepanalysis.js
+++ src/sleepanalysis.js
@@ -1,15 +1,6 @@
-import { parseCSV, groupby, ungroup } from "./data.js"
+import { Data } from "./data.js"
 import { renderChart } from "./chart.js"
 import { renderTable } from "./table.js"
-
-
-export const norms = {
-    totalSleep: {"op": ">", "threshold": 7 * 60 * 60 * 1000},
-    timeToSleep: {"op": "<", "threshold": 30 * 60 * 1000},
-    totalAwake: {"op": "<", "threshold": 90 * 60 * 1000},
-    sleepEfficiency: {"op": ">", "threshold": .85},
-    countsAwake: {"op": "<=", "threshold": 2},
-}
 
 
 export function analyzeSleepPatterns(event, chartId, tableId) {
@@ -20,51 +11,10 @@ export function analyzeSleepPatterns(event, chartId, tableId) {
 }
 
 
-class Data {
-    constructor(values, headers) {
-        this.headers = headers
-        this.values = values
-        this.grouped = groupby(values)
-    }
-
-    static fromCSV(csv) {
-        let {colnames, values} = parseCSV(csv)
-        const [key, datestr, timestr, category] = colnames
-        const headers = {key, datestr, timestr, category}
-        for (const row of values) {
-            row.key = row[key]
-            row.category = row[category]
-            row.timestamp = Data.getTimestampFromDateAndTime(row, headers)
-        }
-        const groups = groupby(values)
-        for (const group of Object.values(groups)) {
-            addNextTimestamp(group)
-            addDurationInMs(group)
-        }
-        values = ungroup(groups)
-        return new Data(values, headers)
-    }
-
-    static getTimestampFromDateAndTime(row, headers) {
-        const {[headers.datestr]: date, [headers.timestr]: time} = row
-        const datestr = `${date.slice(0,4)}-${date.slice(4,6)}-${date.slice(6)}`
-        return Date.parse(`${datestr} ${time}`)
-    }
-}
-
-
-function addNextTimestamp(data) {
-    data.forEach((item, index) => {
-        const nextItem = data[index + 1]
-        item.timestamp2 = nextItem
-            ? nextItem.timestamp
-            : item.timestamp + 300000
-    })
-}
-
-
-function addDurationInMs(data) {
-    data.forEach(item => {
-        item.duration = item.timestamp2 - item.timestamp
-    })
+export const norms = {
+    timeToSleep: {"op": "<", "threshold": 30 * 60 * 1000},
+    totalSleep: {"op": ">", "threshold": 7 * 60 * 60 * 1000},
+    totalAwake: {"op": "<", "threshold": 90 * 60 * 1000},
+    sleepEfficiency: {"op": ">", "threshold": .85},
+    countsAwake: {"op": "<=", "threshold": 2},
 }
